355c38a87e1cbbcd4d20e4ec6471bebd4ae69dd2
clark <dmc950213@gmail.com>
2026-04-30 14:20:33 +0800

    chore: trigger cloudflare deploy

--- src/config.ts
+++ src/config.ts
@@ -1,7 +1,14 @@
+type SocialObjects = {
+  name: string;
+  href: string;
+  linkTitle: string;
+  active: boolean;
+}[];
+
 export const SITE = {
-  website: "https://astro-paper-i18n.netlify.app/", // replace this with your deployed domain
-  author: "Yousef Elsayed",
-  profile: "https://yousef8.github.io/astro-paper-i18n",
+  website: "https://blog.clarkliu.com", // replace this with your deployed domain
+  author: "Clark Liu",
+  profile: "https://clarkliu.com",
   ogImage: "astropaper-og.jpg",
   lightAndDarkMode: true,
   postPerIndex: 4,
@@ -11,9 +18,31 @@ export const SITE = {
   showBackButton: true, // show back button in post detail
   editPost: {
     enabled: true,
-    url: "https://github.com/yousef8/astro-paper-i18n/edit/main/",
+    url: "https://github.com/Clark1945/astro-blog",
   },
   dynamicOgImage: true,
   lang: "en", // html lang code. Set this empty and default will be "en"
-  timezone: "Asia/Bangkok", // Default global timezone (IANA format) https://en.wikipedia.org/wiki/List_of_tz_database_time_zones
+  timezone: "Asia/Taipei", // Default global timezone (IANA format) https://en.wikipedia.org/wiki/List_of_tz_database_time_zones
 } as const;
+
+export const SOCIALS: SocialObjects = [
+  {
+    name: "Github",
+    href: "https://github.com/Clark1945",
+    linkTitle: `${SITE.author} on Github`,
+    active: true,
+  },
+  {
+    name: "LinkedIn",
+    href: "https://www.linkedin.com/in/clark-liu-b48740253/", // 填入你的 LinkedIn
+    linkTitle: `${SITE.author} on LinkedIn`,
+    active: true,
+  },
+  {
+    name: "Twitter",
+    href: "",
+    linkTitle: "",
+    active: false, // 不用的設 false
+  },
+  // 其他 social...
+];
